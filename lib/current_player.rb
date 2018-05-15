07ef566311a0a27fcaa7fed237e8eb1c62146b55
JohnMark <johnmarkludwick@gmail.com>
2018-05-15 12:51:20 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,3 +1,16 @@
-def turn_count(board)
+def current_player(board)
+  if turn_count(board)%2 ==0
+    current_player = "X"
+  else
+    current_player = "O"
+end
+return current_player
+end
+
+
+#MAKE MOVE ON BOARD
+def move(board, index, token)
+  board[index] = token
+  play(board)
   
 end
