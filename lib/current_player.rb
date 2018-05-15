b989960fc2d690bcd82e08c2fe5b7d1bbbc9f3c7
JohnMark <johnmarkludwick@gmail.com>
2018-05-15 14:02:07 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,6 +1,3 @@
-#Who's turn is it? if there's an X on the board, it's O's turn, and vice versa. 
-
-
 def turn_count(board)
   counter = 0
 
@@ -12,4 +9,16 @@ def turn_count(board)
 end
 
 
-
+def turn_count(board) <- here you have a board being passed in
+ 
+  #lets translate this into your method
+  counter = 0
+  
+  #since we already have access to board we can iterate over it  
+	board.each do |space|
+       #inside the iteration we want to check if the space is either X or O
+      if the space of the board is either "X" or "O"
+       counter += 1
+      end
+    end
+end
