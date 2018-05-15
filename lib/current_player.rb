8bb985ab33b93a4649f215ab7692a373d5cdd32e
JohnMark <johnmarkludwick@gmail.com>
2018-05-15 13:59:24 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -4,25 +4,12 @@
 def turn_count(board)
   counter = 0
 
-board.each do |space|
-if the space of the board is either "X" or "O"
-   counter += 1
+  board.each do |space|
+    if the space of the board is either "X" or "O"
+      counter += 1
     end
   end
 end
 
 
-def turn_count(board) <- here you have a board being passed in
- 
-  #lets translate this into your method
-  counter = 0
-  
-  #since we already have access to board we can iterate over it  
-	board.each do |space|
-       #inside the iteration we want to check if the space is either X or O
-      if the space of the board is either "X" or "O"
-       counter += 1
-      end
-    end
-end
 
